3528f3143ea1aa28caef468b49cb990840d3a7ed
Jean-Baptiste Lab <jean.baptiste@space-inventor.com>
2024-03-05 09:34:38 +0100

    Implement and use payload interface

--- src/cspftp_server.c
+++ src/cspftp_server.c
@@ -60,16 +60,12 @@ static uint32_t compute_transfer_size(cspftp_server_transfer_ctx_t *ctx) {
 
 #define PKT_SIZE (200U)
 
-static char dummy_payload[PKT_SIZE - sizeof(uint32_t)];
-
 extern cspftp_result start_sending_data(cspftp_server_transfer_ctx_t *ctx)
 {
     cspftp_result result = CSPFTP_OK;
     csp_packet_t *packet;
-    // TODO: get payload data from somewhere
     uint32_t bytes_sent = 0;
     uint32_t nof_csp_packets = 0;
-    memset(dummy_payload, 0x33, sizeof(dummy_payload));
     dbg_log("Start sending data");
     for(uint8_t i = 0; i < ctx->request.nof_intervals; i++)
     {
@@ -95,8 +91,8 @@ extern cspftp_result start_sending_data(cspftp_server_transfer_ctx_t *ctx)
                 packet->length = (bytes_in_interval - sent_in_interval) + sizeof(uint32_t);
                 dbg_warn("last packet->length= %lu", packet->length);
             }
-            memcpy(packet->data + sizeof(uint32_t), dummy_payload, sizeof(dummy_payload));
             memcpy(packet->data, &bytes_sent, sizeof(uint32_t));
+            ctx->payload_meta.read(ctx->request.payload_id, bytes_sent, packet->data + sizeof(uint32_t), packet->length - sizeof(uint32_t));
             bytes_sent += packet->length - sizeof(uint32_t);
             sent_in_interval += packet->length - sizeof(uint32_t);
             csp_sendto(CSP_PRIO_NORM, ctx->destination, 8, 0, 0, packet);
--- src/payload_api.c
+++ src/payload_api.c
@@ -1,4 +1,17 @@
+#include <string.h>
 #include <cspftp/platform.h>
+
+static uint32_t read(uint8_t payload_id, uint32_t offset, void *output, uint32_t size) {
+    uint32_t res = 0;
+    switch(payload_id) {
+        case 0:
+        default:
+            memset(output, 0x30 + payload_id, size);
+            res = size;
+        break;
+    }
+    return res;
+}
 
 bool get_payload_meta(dftp_payload_meta_t *meta, uint8_t payload_id) {
     bool result = true;
@@ -8,6 +21,7 @@ bool get_payload_meta(dftp_payload_meta_t *meta, uint8_t payload_id) {
         default:
             /* 128 Mb for testing */
             meta->size = 128 * 1024 * 1024;
+            meta->read = read;
         break;
     }
     return result;
