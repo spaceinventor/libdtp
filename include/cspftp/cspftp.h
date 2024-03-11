98c50a8fcc47b62dbcb154c8097dc163c30e3d19
Jean-Baptiste Lab <jean.baptiste@space-inventor.com>
2024-03-11 15:44:31 +0100

    Add CSPFTP version constant to interface

--- include/cspftp/cspftp.h
+++ include/cspftp/cspftp.h
@@ -118,6 +118,7 @@ extern "C"
     
     /** Default session hooks, define this variable if you want your own implementation */
     extern cspftp_opt_session_hooks_cfg default_session_hooks;
+    extern const uint32_t CSPFTP_SESSION_VERSION;
 
     extern const uint16_t CSPFTP_PACKET_SIZE;
 
