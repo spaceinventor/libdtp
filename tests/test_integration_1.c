ff7afac79110df88feda83ed3944412fef5efd2c
Jean-Baptiste Lab <jean.baptiste@space-inventor.com>
2024-03-12 15:04:49 +0100

    Revert test_integration_1 for now, add *.bin to gitignore

--- tests/test_integration_1.c
+++ tests/test_integration_1.c
@@ -79,9 +79,9 @@ VMEM_DEFINE_FILE(dtp_test_session, "dtp_test_session.json", "dtp_test_session.js
 
 REGISTER_TEST(in_process_transfer) {
 	dtp_t *session;
-	// TEST_ASSERT(0 == dtp_client_main(0, 1024 * 63, 2, false, &session));
-	// dtp_serialize_session(session, 0);
-	TEST_ASSERT(0 == dtp_client_main(0, 1024 * 63, 2, true, &session));
+	TEST_ASSERT(0 == dtp_client_main(0, 1024 * 63, 2, false, &session));
+	dtp_serialize_session(session, 0);
+	// TEST_ASSERT(0 == dtp_client_main(0, 1024 * 63, 2, true, &session));
 	// dtp_serialize_session(session, &vmem_dtp_test_session);
 }
 
