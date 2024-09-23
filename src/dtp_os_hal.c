f5dc134fc5781190f103c5c5e710e4977e23d979
Thomas Lykkeberg <thomas.lykkeberg@space-inventor.com>
2024-09-23 09:32:46 +0200

    SI-5007 Added more POSIX implementation

--- src/dtp_os_hal.c
+++ src/dtp_os_hal.c
@@ -4,7 +4,7 @@
 #include "csp/autoconfig.h"
 
 #if defined (CSP_POSIX)
-#error "The OS hal for poll'ing is not implemented for POSIX platforms"
+#include "csp/csp.h"
 #elif defined (CSP_FREERTOS)
 #include "FreeRTOS.h"
 #include "task.h"
@@ -14,11 +14,20 @@
 
 void os_hal_start_poll_operation(uint32_t poll_period_ms, bool (*poll)(uint32_t op, void *), void *context) {
 
-    TickType_t wakeupTime = xTaskGetTickCount();
-    TickType_t ticks = pdMS_TO_TICKS(poll_period_ms);
     bool carryon = true;
 
-    printf("Poll: Start polling function at: %p every %"PRIu32" ms\n", poll, ticks * configTICK_RATE_HZ);
+#if defined (CSP_POSIX)
+    uint32_t current_time = csp_get_ms();
+    /* On the POSIX platform, this is just a tight loop */
+    while (carryon) {
+        /* Calculate the sleep period */
+        carryon = (*poll)(1, context);
+    }
+#elif defined (CSP_FREERTOS)
+    TickType_t wakeupTime = xTaskGetTickCount();
+    TickType_t ticks = pdMS_TO_TICKS(poll_period_ms);
+
+    printf("Poll: Start polling function at: %p every %"PRIu32" ms\n", poll, ticks);
 
     while (carryon) {
         BaseType_t xWasDelayed = xTaskDelayUntil(&wakeupTime, ticks);
@@ -28,5 +37,6 @@ void os_hal_start_poll_operation(uint32_t poll_period_ms, bool (*poll)(uint32_t 
         /* Call the poll method passed in as argument */
         carryon = (*poll)(1, context);
     }
+#endif
 
 }
