f9bc8cfc8de2bd9c0498a9fa00173a2d796e414c
Thomas Lykkeberg <thomas.lykkeberg@space-inventor.com>
2025-04-08 14:40:28 +0200

    Using libossi rather than libsi.

--- src/dtp_vmem_server.c
+++ src/dtp_vmem_server.c
@@ -7,7 +7,7 @@
 #include "dtp/dtp_async_api.h"
 #include "dtp/dtp_vmem_server.h"
 
-#include "message_queue.h"
+#include "ossi/message_queue.h"
 
 #include "vmem/vmem.h"
 #include "vmem/vmem_server.h"
