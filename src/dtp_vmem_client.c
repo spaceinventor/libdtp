c5a2d368bbe04289cd1e023014f2cdabe3d8dacd
Thomas Lykkeberg <thomas.lykkeberg@space-inventor.com>
2025-04-08 16:07:13 +0200

    Moved out the target specific handling of the DTP on_data handling. This is now located in the csh APM.

--- src/dtp_vmem_client.c
+++ src/dtp_vmem_client.c
@@ -9,28 +9,7 @@
 #include "dtp/dtp_vmem_server.h"
 #include "dtp/dtp_session.h"
 
-#define STRINGIFY_IMPL(arg) #arg
-#define STRINGIFY(arg) STRINGIFY_IMPL(arg)
-
-#define CSI "\x1B["
-#define SAVE_CURPOS CSI "s"
-#define RESTORE_CURPOS CSI "u"
-#define ERASE_LINE CSI "K"
-#define SET_CURPOS CSI "%u;%uH"
-#define SCROLL_UP CSI "S"
-#define SCROLL_DOWN CSI "T"
-
-static bool vmem_dtp_on_data_packet(dtp_t *session, csp_packet_t *packet)
-{
-    printf(SAVE_CURPOS);
-    printf(SET_CURPOS, 0, 0);
-    printf(ERASE_LINE);
-    printf("Receiving: [%"PRIu32":%"PRIu32"]\n", packet->data32[0], (uint32_t)(packet->data32[0] + packet->length - sizeof(uint32_t) - 1));
-    printf(RESTORE_CURPOS);
-    return true;
-}
-
-int vmem_dtp_download(int node, int timeout, uint64_t address, uint32_t length, char * dataout, int version, int use_rdp, uint32_t thrughput)
+int vmem_dtp_download(int node, int timeout, uint64_t address, uint32_t length, vmem_dtp_on_data_t *on_data, int version, int use_rdp, uint32_t thrughput)
 {
     uint8_t intervals = 1;
 
@@ -112,7 +91,8 @@ int vmem_dtp_download(int node, int timeout, uint64_t address, uint32_t length, 
     }
 
     session->payload_size = length;
-    session->hooks.on_data_packet = vmem_dtp_on_data_packet;
+    session->hooks.on_data_packet = on_data;
+    session->hooks.hook_ctx = NULL;
     res = start_receiving_data(session);
     if (res != DTP_OK) {
         printf("Error receiving data: %d\n", res);
